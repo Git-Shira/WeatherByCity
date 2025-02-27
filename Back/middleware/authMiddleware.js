b9d0bc2928cb0600781a0c8427a2dd28c8ca9349
Git-Shira <shirae447@gmail.com>
2025-02-27 16:28:08 +0200

    Added auth middleware

---
+++ Back/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+const { v4: uuidv4 } = require('uuid');
+const User = require('../models/User');
+
+const authMiddleware = async (req, res, next) => {
+    try {
+        let userUUID = req.cookies.userUUID;
+        console.log(userUUID);
+        if (!userUUID) {
+            userUUID = uuidv4();
+            res.cookie('userUUID', userUUID, { maxAge: 365 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict' });
+
+            await User.create({ uuid: userUUID, locations: [] });
+        } else {
+            res.cookie('userUUID', userUUID, { maxAge: 365 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict' });
+
+            await User.updateOne(
+                { uuid: userUUID },
+                { $set: { expiresAt: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000) } }
+            );
+        }
+
+        req.userUUID = userUUID;
+        console.log(req.userUUID);
+        next();
+    } catch (error) {
+        console.error("Error in authMiddleware:", error);
+        res.status(500).send({ message: "Internal Server Error" });
+    }
+};
+
+module.exports = authMiddleware;
