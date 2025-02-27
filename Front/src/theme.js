3f9f0896a2addc4e091e340aedf6aab81ff7a603
Git-Shira <shirae447@gmail.com>
2025-02-27 16:32:13 +0200

    Added theme file for RTL support

---
+++ Front/src/theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material/styles";
+import rtlPlugin from "stylis-plugin-rtl";
+import { prefixer } from "stylis";
+import createCache from "@emotion/cache";
+
+export const theme = createTheme({
+    direction: "rtl",
+});
+
+export const cacheRtl = createCache({
+    key: "muirtl",
+    stylisPlugins: [prefixer, rtlPlugin],
+});
